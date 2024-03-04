3494023589fe99236e25b3ba407d36434bfa6266
shinhwiii <sh0122451@gmail.com>
2024-03-04 18:36:26 +0900

    feat: 오른손 법칙

---
+++ Algorithm/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Board board = new Board();
+            Player player = new Player();
+            board.Initialize(25, player);
+            player.Initialize(1, 1, board);
+
+            Console.CursorVisible = false;
+
+            const int WAIT_TICK = 1000 / 30;
+
+            int lastTick = 0;
+
+            while (true)
+            {
+                #region 프레임 관리
+                int currentTick = System.Environment.TickCount;
+                // 만약 경과한 시간이 1/30보다 작다면
+                if (currentTick - lastTick < WAIT_TICK)
+                    continue;
+                int deltaTick = currentTick - lastTick;
+                lastTick = currentTick;
+                #endregion
+
+                // 입력
+
+                // 로직
+                player.Update(deltaTick);
+
+                // 렌더링
+                Console.SetCursorPosition(0, 0);
+                board.Render();
+            }
+        }
+    }
+}
